dbc5378443f84d1c5d9371c3e8722bf4065c1c9e
Timur Simonov <tsimonov12@gmail.com>
2026-01-18 20:56:27 +0300

    dataclasses is so perfect

--- test_print_func.py
+++ test_print_func.py
@@ -1,3 +1,18 @@
-def test_print_func(text: str) -> str:
-  text.capitalize()
-  print(text)
+from dataclasses import dataclass
+
+@dataclass
+class DB_Config:
+    host: str
+    user: str
+    password: str
+    database: str
+
+@dataclass
+class Bot:
+    token: str
+    admin_ids: list[str]
+  
+@dataclass
+class Config:
+    bot: Bot
+    db: DB_Config
